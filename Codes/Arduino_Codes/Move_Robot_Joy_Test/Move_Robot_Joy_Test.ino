32f5ae32ca1718d2e00ddac3772d9cb0947ea8ca
SerchHersa <i.sergio.hersa@gmail.com>
2025-04-24 15:07:55 -0600

    Corrección lógica joystick para movimiento

--- Codes/Arduino_Codes/Move_Robot_Joy_Test/Move_Robot_Joy_Test.ino
+++ Codes/Arduino_Codes/Move_Robot_Joy_Test/Move_Robot_Joy_Test.ino
@@ -11,8 +11,8 @@
 #define RMot_pwm 15     // PWM2 Cytron 2 Channel
 
 // Pines de conexión de los motores de la barredora
-#define Sweeper_dir 5      // DIR Cytron 1 Channel
-#define Sweeper_pwm 17     // PWM Cytron 1 Channel
+#define Intake_dir 5      // DIR Cytron 1 Channel
+#define Intake_pwm 17     // PWM Cytron 1 Channel
 
 // Pines de señal para los servomotores
 
@@ -20,14 +20,14 @@
  int velRob = 0;
  int angRob = 0;
 
- int velSweeper = 0;
+ int velIntake = 0;
 
 // ---------------------------- Setup -----------------------------------------
 void setup() {
   // Configuración de pines como salida o entrada
   pinMode(LMot_dir, OUTPUT);
   pinMode(RMot_dir, OUTPUT);
-  pinMode(Sweeper_dir, OUTPUT);
+  pinMode(Intake_dir, OUTPUT);
   
   // Inicializando PS4 y Serial
   PS4.begin();
@@ -45,12 +45,12 @@ void loop() {
     angRob = PS4.LStickX();         // Leyendo valor de Joystick izquierdo al moverlo izquierda/derecha
     angRob = map(angRob, -127, 127, -200, 200);     // Escalando
     
-    velSweeper = PS4.RStickY();         // Leyendo valor de Joystick derecho al moverlo arriba/abajo 
-    velSweeper = map(velSweeper, -127, 127, -255, 255);     // Escalando
+    velIntake = PS4.RStickY();         // Leyendo valor de Joystick derecho al moverlo arriba/abajo 
+    velIntake = map(velIntake, -127, 127, -255, 255);     // Escalando
     
     moveRobot(velRob, angRob);
 
-    moveSweeper(velSweeper);
+    moveIntake(velIntake);
 
   delay(10);         // Esperar 2 segundos
   }
@@ -59,7 +59,7 @@ void loop() {
     analogWrite(LMot_pwm, 0);
     analogWrite(RMot_pwm, 0);
 
-    analogWrite(Sweeper_pwm, 0);
+    analogWrite(Intake_pwm, 0);
   }
 
   
@@ -67,63 +67,79 @@ void loop() {
 
 
 // ---------------------------- Functions -----------------------------------------
-void moveRobot(int velocidadPWM, int direccion) {
+void moveRobot(int velocidadPWM, int giroPWM) {
   // Variables para calcular las señales PWM de cada motor
   int pwmIzquierdo = 0;
   int pwmDerecho = 0;
 
-  pwmIzquierdo = velocidadPWM + direccion;       // Calculando velocidad de la rueda izquierda
+  pwmIzquierdo = velocidadPWM + giroPWM;       // Calculando velocidad de la rueda izquierda
   pwmIzquierdo = constrain(pwmIzquierdo, -255, 255);    // Restringuiendo límites para rueda izquierda
-  pwmDerecho = velocidadPWM - direccion;       // Calculando velocidad de la rueda derecha
+  pwmDerecho = velocidadPWM - giroPWM;       // Calculando velocidad de la rueda derecha
   pwmDerecho = constrain(pwmDerecho, -255, 255);    // Restringuiendo límites para rueda derecha
 
   // Condiciones para establecer la dirección del giro
-  if (pwmIzquierdo > 10 && pwmDerecho < -10) {
+  if (pwmIzquierdo < -20 && pwmDerecho > 20) {
     // Giro a la izquierda
     digitalWrite(LMot_dir,HIGH);
     digitalWrite(RMot_dir,LOW);
   } 
-  else if (pwmIzquierdo < -10 && pwmDerecho > 10) {
+  else if (pwmIzquierdo > 20 && pwmDerecho < -20) {
     // Giro a la derecha
     digitalWrite(LMot_dir,LOW);
     digitalWrite(RMot_dir,HIGH);
   }
-  else if (pwmIzquierdo > 10 && pwmDerecho > 10){
+  else if (pwmIzquierdo > 20 && pwmDerecho > 20){
     // Movimiento recto y hacia adelante
     digitalWrite(LMot_dir,LOW);
     digitalWrite(RMot_dir,LOW);
   }
-  else if (pwmIzquierdo < -10 && pwmDerecho < -10){
+  else if (pwmIzquierdo < -20 && pwmDerecho < -20){
     // Movimiento recto y hacia atrás
     digitalWrite(LMot_dir,HIGH);
     digitalWrite(RMot_dir,HIGH);
   }
-    
-  // Aplicar señales PWM a los motores
-  analogWrite(LMot_pwm, abs(pwmIzquierdo));
-  analogWrite(RMot_pwm, abs(pwmDerecho));
 
-  /*Serial.print(velocidadPWM);  // Imprimiendo Dato de Movimiento Lineal del Robot
+  if(pwmIzquierdo > -20 && pwmIzquierdo < 20){
+    // Aplicar señales PWM a los motores
+    analogWrite(LMot_pwm, 0);
+    
+    Serial.println("detenidoLEFT");
+  }
+  else{
+    // Aplicar señales PWM a los motores
+    analogWrite(LMot_pwm, abs(pwmIzquierdo));
+  }
+
+  if(pwmDerecho > -20 && pwmDerecho < 20){
+    analogWrite(RMot_pwm, 0);
+    Serial.println("detenidoRIGHT");
+  }
+  else{
+    // Aplicar señales PWM a los motores
+    analogWrite(RMot_pwm, abs(pwmDerecho));
+  }
+
+  Serial.print(velRob);  // Imprimiendo Dato de Movimiento Lineal del Robot
   Serial.print(" / ");   
-  Serial.print(direccion);  // Imprimiendo Dato de Movimiento Angular del Robot
-  Serial.print(" /---/ ");
+  Serial.print(angRob);  // Imprimiendo Dato de Movimiento Angular del Robot
+  Serial.print("  / --- /  ");
   Serial.print(pwmIzquierdo);  // Imprimiendo Dato para rueda izquierda
   Serial.print(" / ");  
-  Serial.println(pwmDerecho);  // Imprimiendo Dato para rueda izquierda*/
+  Serial.println(pwmDerecho);  // Imprimiendo Dato para rueda izquierda
 }
 
-void moveSweeper(int velocidadPWM) {
+void moveIntake(int velocidadPWM) {
   // Condiciones para establecer la dirección del giro
   if (velocidadPWM > 0) {
     // Mete latas
-    digitalWrite(Sweeper_dir,HIGH);
+    digitalWrite(Intake_dir,HIGH);
   } 
   else {
     // Saca latas
-    digitalWrite(Sweeper_dir,LOW);
+    digitalWrite(Intake_dir,LOW);
   } 
   velocidadPWM = constrain(velocidadPWM, -255, 255);    // Restringuiendo límites para motor
-  analogWrite(Sweeper_pwm, abs(velocidadPWM));          // Aplicar señal PWM al motor
+  analogWrite(Intake_pwm, abs(velocidadPWM));          // Aplicar señal PWM al motor
 
   Serial.println(velocidadPWM);  // Muestra el valor que se manda al motor de la barredora*/
 }
